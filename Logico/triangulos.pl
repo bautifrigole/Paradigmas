c5f2046b55f23657ae5b3ffd4bb93a0f02489240
Bautista Frigolé <bautistafrigole@gmail.com>
2023-10-03 21:28:05 -0300

    Added alternative solution triangulos.pl

--- Logico/triangulos.pl
+++ Logico/triangulos.pl
@@ -1,3 +1,4 @@
+% Solución 1
 mayor_cero(X,Y,Z):- X>0, Y>0, Z>0.
 suma_dos_lados(X,Y,Z):- X+Y >= Z, X+Z >= Y, Y+Z >= X.
 
@@ -9,32 +10,15 @@ tipo_triangulo(X,Y,X,T):- triangulo(X,Y,X), X \== Y, T = "isosceles", !.
 tipo_triangulo(X,Y,Y,T):- triangulo(X,Y,Y), Y \== X, T = "isosceles", !.
 tipo_triangulo(X,Y,Z,T):- triangulo(X,Y,Z), T = "escaleno", !.
 
-divFromTo([H|T], I , F, T, L):- I<F, I1 is I+1, divFromTo(T,I1,F,T,L).
-divFromTo([H|T], I , F, T, [H|L]):- I>=F, I<T, ! , I1 is I+1, divFromTo(T,I1,F,T,L).
-divFromTo(_,_,_,_,[]).
+% Solución 2
+mayor_cero(X,Y,Z):- X>0, Y>0, Z>0.
+suma_dos_lados(X,Y,Z):- X+Y >= Z, X+Z >= Y, Y+Z >= X.
 
-%Otro caso con puntos 
+es_triangulo(X,Y,Z):- mayor_cero(X,Y,Z), suma_dos_lados(X,Y,Z).
 
-conectada(a, b).
-conectada(a, c).
-conectada(b, d).
-conectada(c, e).
-conectada(d, f).
-conectada(e, f).
-conectada(e, b).
-conectada(f, g).
-conectada(g, a).
+tipo_triangulo(X,Y,Z,T):- X=Y, Y=Z, !, T = "equilatero".
+tipo_triangulo(X,Y,_,T):- X=Y, !, T = "isosceles".
+tipo_triangulo(_,Y,Z,T):- Y=Z, !, T = "isosceles".
+tipo_triangulo(_,_,_,T):- T = "escaleno".
 
-% Predicado para encontrar una ruta desde Origen hasta Destino
-encontrar_ruta(Origen, Destino, Camino) :-
-    encontrar_ruta_aux(Origen, Destino, [Origen], Camino).
-
-% Caso base: llegamos al destino
-encontrar_ruta_aux(Destino, Destino, Camino, Camino).
-
-% Caso recursivo: encontramos una conexión intermedia y continuamos la búsqueda.
-encontrar_ruta_aux(Origen, Destino, Camino, CaminoFinal) :-
-    conectada(Origen, Intermedia),
-    \+ member(Intermedia, Camino),  % Evita ciclos.
-    append(Camino, [Intermedia], CaminoExtendido), % Añade la conexión intermedia al camino.
-    encontrar_ruta_aux(Intermedia, Destino, CaminoExtendido , CaminoFinal), !. % Usamos corte porque solo queremos la primera solucion.
+triangulo(X,Y,Z,T):- es_triangulo(X,Y,Z), tipo_triangulo(X,Y,Z,T).
